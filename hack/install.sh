723db1fb215229a628f75c52b8c488f59cc8b10e
Zheng SHAO <z-shao@colopl.co.jp>
2019-10-01 14:27:09 +0900

    allow to use custom image

--- hack/install.sh
+++ hack/install.sh
@@ -3,6 +3,10 @@
 set -ex
 set -o pipefail
 
+DEFAULT_LOCALDNS_IMAGE=axot/k8s-dns-local-dns-amd64:v0.0.1-6-ga977ec7
+
+[ -z ${LOCALDNS_IMAGE} ] && LOCALDNS_IMAGE=${DEFAULT_LOCALDNS_IMAGE}
+
 # Create the underlying uncached service
 kubectl apply -f kube-dns-uncached.yaml
 
@@ -19,7 +23,8 @@ cat k8s-local-dns.yaml configmap.yaml \
   | sed -e s/addonmanager.kubernetes.io/#addonmanager.kubernetes.io/g \
   | sed -e s@kubernetes.io/cluster-service@#kubernetes.io/cluster-service@g \
   | sed -e 's@k8s-app: kube-dns@k8s-app: local-dns@g' \
-  | sed s/__PILLAR__LOCAL__DNS__/${LOCAL_DNS}/g \
-  | sed s/__PILLAR__DNS__SERVER__/${UPSTREAM_DNS}/g \
+  | sed -e s/__PILLAR__LOCAL__DNS__/${LOCAL_DNS}/g \
+  | sed -e s/__PILLAR__DNS__SERVER__/${UPSTREAM_DNS}/g \
   | sed -e s/__PILLAR__DNS__DOMAIN__/${DNS_DOMAIN}/g \
+  | sed -e s@__LOCALDNS_IMAGE__@${LOCALDNS_IMAGE}@ \
   | kubectl apply -f -
